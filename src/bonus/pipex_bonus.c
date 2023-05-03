2bffdff32407a673650c15631732d53742d4128b
Gergely Pasztor <gpasztor@3-G-11.42heilbronn.de>
2023-05-03 15:52:32 +0200

    here_doc working

--- src/bonus/pipex_bonus.c
+++ src/bonus/pipex_bonus.c
@@ -6,11 +6,46 @@
 /*   By: gpasztor <gpasztor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 10:54:31 by gpasztor          #+#    #+#             */
-/*   Updated: 2023/05/03 13:25:35 by gpasztor         ###   ########.fr       */
+/*   Updated: 2023/05/03 15:44:58 by gpasztor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/pipex_bonus.h"
+
+void	here_doc_loop(int tempfd, int filefd, char *delim)
+{
+	char	*line;
+
+	line = NULL;
+	while (1)
+	{
+		line = get_next_line(filefd);
+		if (line == NULL || ft_strncmp(line, delim, ft_strlen(delim)) == 0)
+			break ;
+		ft_fprintf(tempfd, "%s", line);
+		free(line);
+	}
+	if (line != NULL)
+		free(line);
+}
+
+void	here_doc(char *file, char *delim)
+{
+	int		filefd;
+	int		tempfd;
+
+	filefd = open(file, O_RDONLY, 0644);
+	if (filefd == -1)
+		error("pipex: input", errno);
+	tempfd = open(".heredoc0.XXXXXX", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	dup2(filefd, STDIN_FILENO);
+	here_doc_loop(tempfd, filefd, delim);
+	close(filefd);
+	close(tempfd);
+	tempfd = open(".heredoc0.XXXXXX", O_RDONLY, 0644);
+	dup2(tempfd, STDIN_FILENO);
+	unlink(".heredoc0.XXXXXX");
+}
 
 void	children(char *rawcmd, char **envp)
 {
@@ -75,10 +110,19 @@ int	main(int argc, char **argv, char **envp)
 
 	if (argc >= 5)
 	{
-		cmdi = 2;
-		infile = open(argv[1], O_RDONLY, 0644);
-		outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		dup2(infile, STDIN_FILENO);
+		if (ft_strncmp(argv[1], "here_doc", 8) == 0)
+		{
+			cmdi = 4;
+			outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+			here_doc(argv[2], argv[3]);
+		}
+		else
+		{
+			cmdi = 2;
+			infile = open(argv[1], O_RDONLY, 0644);
+			outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			dup2(infile, STDIN_FILENO);
+		}
 		while (cmdi < argc - 2)
 			children(argv[cmdi++], envp);
 		last_cmd(argv[argc - 2], envp, outfile);
